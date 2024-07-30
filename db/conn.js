ea10a07235d313628477322ff9822e6deca9d8be
Tarik Maljanovic <tarikmaljanovic123@gmail.com>
2024-07-30 16:37:49 +0200

    .

--- db/conn.js
+++ db/conn.js
@@ -1,7 +1,6 @@
 import { MongoClient } from "mongodb";
 
 const connectionString = process.env.ATLAS_URI;
-console.log(connectionString);
 
 const client = new MongoClient(connectionString);
 
@@ -11,11 +10,6 @@ try {
     conn = await client.connect();
 } catch (err) {
     console.error(err);
-}
-
-if(process.env.NODE_ENV === "development") {
-    conn.db("portfolio").createCollection("portfolio");
-    conn.db("portfolio").createCollection("users");
 }
 
 let db = conn.db("portfolio");
